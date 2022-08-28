e2e38d325af7bc175758d3fe055c4c47d29d1359
manoj <manoj.reddy66336688@gmail.com>
2022-08-28 15:53:33 +0530

    first commit

---
+++ JavaProblems/src/problemsofjava/GoodPractice.java
@@ -0,0 +1,20 @@
+package problemsofjava;
+
+public class GoodPractice {
+
+	public static void main(String[] args) {
+
+		int a=40;
+		
+		if(a<35) {
+			System.out.println("it is true");
+			
+		}
+		else {
+			
+			System.out.println("it is false you need to work");
+		}
+		
+	}
+
+}
---
+++ JavaProblems/src/problemsofjava/Secondjava.java
@@ -0,0 +1,16 @@
+package problemsofjava;
+
+public class Secondjava {
+
+	public static void main(String[] args) {
+
+		String name="mynameismanoj";
+		
+		StringBuffer sb=new StringBuffer(name);
+		StringBuffer rev=sb.reverse();
+		System.out.println(rev);
+		
+		
+	}
+
+}
